505a4db5cf1ea3e5ac3f8159959bfa69dc534609
rafat22 <rafat22@navgurukul.org>
2024-11-04 20:39:57 +0530

    put printassist here

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"OqSG+bix3WCX8TF7R/GMTNtJipIGrSSGMXsXOnFqtIs=\"\n}"
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,34 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        customGreen: '#0ac17c',
+        customBlue: '#3fa4d7',
+      },
+      keyframes: {
+        float: {
+          '0%, 100%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(-20px)' },
+        },
+      },
+      animation: {
+        float: 'float 4s ease-in-out infinite',
+      },
+      fontFamily: {
+        dancing: ['"Dancing Script"', 'cursive'],
+      },
+    },
   },
   plugins: [],
 };
